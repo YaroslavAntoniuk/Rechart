8fbb5c9375ca9725e54e4e8505e0bbd5cfe18b6f
YaroslavAntoniuk <yaroslav.antoniuk@binary-studio.com>
2023-10-13 18:37:27 +0100

    Added charts to the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import Recharts from './components/Recharts';
+import NotFoundPage from './components/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div style={{ height: '100vh' }}>
+          <Routes>
+            <Route exact path="/" element={<Recharts />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
